9d2435df0ccc5aaf928718ad47d16e07474694b3
cnune <cnunez@capw.com.do>
2025-08-04 13:06:33 -0400

    [FIX] CUARTO COMMIT

--- mis_builder_cash_flow/migrations/16.0.1.0.0/post-migration.py
+++ mis_builder_cash_flow/migrations/16.0.1.0.0/post-migration.py
@@ -1,10 +1,10 @@
 # Copyright 2024 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openupgradelib import openupgrade
+from odoo import api, SUPERUSER_ID
 
-
-@openupgrade.migrate()
-def migrate(env, version):
-    openupgrade.load_data(
-        env, "mis_builder_cash_flow", "migrations/16.0.1.0.0/noupdate_changes.xml"
+def migrate(cr, version):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env.ref('mis_builder_cash_flow.noupdate_changes', False)
+    env['ir.model.data']._load_xml_files(
+        'mis_builder_cash_flow', ['migrations/16.0.1.0.0/noupdate_changes.xml']
     )
